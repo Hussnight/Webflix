5bec8b5ade1b1c290781524db4cf3c39e1292d83
Hocine <agouni.hocine@gmail.com>
2018-10-29 17:22:12 +0100

    à jour

--- formulaire.php
+++ formulaire.php
@@ -1,19 +1,10 @@
 <?php // Inclusion de header.php sur la page
 require_once(__DIR__.'/partials/header.php'); ?>
-<?php require_once(__DIR__.'/config/database.php') ?>
 
 <main>
   <h1 class="page-title">Bienvenue sur WebfLix !</h1>
   <div class="separation-page-title"></div>
 </main>
 
-
-
-
 <?php // Inclusion de footer.php sur la page
 require_once(__DIR__.'/partials/footer.php');?>
-
-
-
-
-
--- partials/header.php
+++ partials/header.php
@@ -9,7 +9,7 @@
     <title>Document</title>
 </head>
 <body>
-    
+  
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="index.php">Webflix</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" >
@@ -21,12 +21,16 @@
       <li class="nav-item active">
         <a class="nav-link" href="">Accueil</a>
       </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="formulaire.php">Ajouter un film</a>
+      </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
     </form>
   </div>
 </nav>
+
 
  
