4eca7dba904b9ba64d188aa740be4443b8e88f82
Hocine <agouni.hocine@gmail.com>
2018-10-30 16:34:59 +0100

    à jour

--- config/database.php
+++ config/database.php
@@ -1,2 +1,8 @@
 <?php
-$db = new PDO('mysql:host=localhost; dbname=pizzastore; charset=utf8',  'root', '');
+$db = new PDO('mysql:host=localhost; dbname=webflix; charset=utf8',  'root', '');
+
+$query = $db->query('SELECT * FROM category');
+$categories = $query->fetchAll();
+
+$query = $db->query('SELECT * FROM movie');
+$movies = $query->fetchAll();
--- index.php
+++ index.php
@@ -2,11 +2,32 @@
 require_once(__DIR__.'/partials/header.php'); ?>
 <?php require_once(__DIR__.'/config/database.php') ?>
 
+
 <main>
-  <h1 class="page-title">Bienvenue sur WebfLix !</h1>
+  <h1 class="page-title">Bienvenue sur WebfLix !</h1> <br>
+  <h2 class="main-subtitle">Merci d'utiliser notre interface afin de visionner les films de votre choix.</h2>
   <div class="separation-page-title"></div>
 </main>
 
+<section>
+<h2 class="category-style">Catégories <br><br> <?php  
+foreach($categories as $categorie){
+      echo $categorie['name'].'<br>';
+
+      foreach($movies as $movie){
+        if ($movie['category_id'] === $categorie['id'])
+           echo '<div class="col-md-3">
+                    <div class="card mb-4">
+                    <div class="card-img-top-container">
+                      <img class="card-img-top card-img-top-zoom-effect" src="assets/'.$movie['cover'].'"alt="Card image cap">
+                </div>
+                    </div>
+                    </div>';
+      }
+    }
+
+?></h2>
+</section>
 
 
 
